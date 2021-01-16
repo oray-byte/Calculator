9c77e428df42f86ec60f267088df646149fda716
oray-byte <56564940+oray-byte@users.noreply.github.com>
2021-01-15 18:20:29 -0600

    Fixed bugs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Calculator
-Personal Project
+# Python-based Calculator
+Personal project started during winter break of 2020-2021
+
+## Why?
+I was bored and got inspired by a video on Youtube of a develepor creating a web-based calculator.
+**The code is written from scratch with no tutorials**
+
+## Inspiration
+As previously mentioned, I was inspired by a video on Youtube; however, that was not my only source of inspiration.
+Recently, I discovered the tkinter module in Python. While I am more comfortable in the back, I enjoy building something that doesn't require the console to work.
--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ Created on Sun Jan  3 14:40:03 2021
 """
 
 import tkinter as tkr
-import tkinter.messagebox as tkrm
+import tkinter.messagebox as tkrm # Using as a debug tool
 import tkinter.font as tkrf
 from typing import List
 
@@ -21,139 +21,77 @@ class Calculator(tkr.Frame):
         tkr.Frame.pack(self)
         self.init_frame()
 
-    def _on_enter(self, widget):
+# ------Button Highlights------------------------------------------------------
+    def _on_enter(self, widget) -> None:
         widget.widget["background"] = "DarkOrchid3"
     
-    def _on_leave(self, widget):
+    def _on_leave(self, widget) -> None:
         widget.widget["background"] = "gray19"
+# -----------------------------------------------------------------------------
 
-    def _format_string(self, temp):
+# ------Useful Functions-------------------------------------------------------
+    def _clear(self, *args) -> None:
+        for canvas in args:
+            canvas.delete("all")
+
+    def _format_string(self, temp) -> str:
         
         # FIXME Add functionality to chop off unnessessary zeros
         # FIXME Add functionality to add commas for each 3 digits before .
-        return "{:.0f}".format(temp)
+        return "{:.2f}".format(temp)
 
-    def _operator_input(self, function, topCanvas, bottomCanvas, operator):
+    def _operator_input(self, function, topCanvas, bottomCanvas, operator) -> None:
         if self.current != '':
-            topCanvas.delete("all")
-            bottomCanvas.delete("all")
+            self._clear(topCanvas, bottomCanvas)
             self.previous = self.current + " {}".format(operator)
             self.current = ''
             function(self.previous, topCanvas)
             self.operator = operator
         else:
-            topCanvas.delete("all")
+            self._clear(topCanvas)
             self.previous = self.previous + " {}".format(operator)
             function(self.previous, topCanvas)
             self.operator = operator
         
-    def _do_math(self, function, topCanvas, bottomCanvas):
+    def _do_math(self, function, topCanvas, bottomCanvas, operator=None):
         temp: float = 0
-        if self.previous != '':
-            if self.operator == "+":
+        if operator == "x²":
+            if self.current != '':
+                temp = float(self.current) ** 2
+            else:
+                temp = float(self.previous) ** 2
+        if self.previous != '' and self.previous != 'x²':
+            if operator == "+":
                 temp = float(self.previous.split()[0]) + float(self.current)
-            elif self.operator == "-":
+            elif operator == "-":
                 temp = float(self.previous.split()[0]) - float(self.current)
-            elif self.operator == "x":
+            elif operator == "x":
                 temp = float(self.previous.split()[0]) * float(self.current)
-            elif self.operator == "x²":
-                if self.current != '':
-                    temp = float(self.current) ** 2
+            elif operator == "÷":
+                if self.current == '0':
+                    self._clear(topCanvas, bottomCanvas)
+                    self.current = ''
+                    self.previous = ''
+                    self.operator = ''
+                    tkrm.showinfo("Error", "Cannot divide by zero")
+                    return
                 else:
-                    temp = float(self.previous) ** 2
-            else: # FIXME add precaution when user divides by zero
-                temp = float(self.previous.split()[0]) / float(self.current)
+                    temp = float(self.previous.split()[0]) / float(self.current)
 
         self.previous = self._format_string(temp)
         self.current = ''
         self.operator = ''
-        bottomCanvas.delete("all")
-        topCanvas.delete("all")
+        self._clear(topCanvas, bottomCanvas)
         function(self.previous, topCanvas)
         
-    def _append_to_frame(self, btn, topCanvas, bottomCanvas) -> None:
-        """
-        Takes the value of the pressed-button's text and appends it
-        to the current text of the canvas.
-        """
-        canvasFont: tkrf.Font = tkrf.Font(self, family="Stencil", size="25")
-
-        c_text = (lambda text, canvas:
-                  canvas.create_text(390, 50, text=text, fill="snow",
-                                     font=canvasFont, anchor=tkr.E))
-
-        if ((str(btn.cget("text")).isnumeric()) or ((str(btn.cget("text")) == ".") and (self.current.count(".") < 1))):
-            self.current = self.current + str(btn.cget("text"))
-            bottomCanvas.delete("all")
-            c_text(self.current, bottomCanvas)
-            
-        # Handles functionality when DEL button is pressed
-        if btn.cget("text") == "DEL":
-            self.current = self.current[:-1]
-            bottomCanvas.delete("all")
-            c_text(self.current, bottomCanvas) 
-
-        # Handles functionality when ± button is pressed
-        if btn.cget("text") == "±":
-            if (self.current != '-' or self.current == '') and self.current.count("-") < 1:
-                self.current = '-' + self.current
-                bottomCanvas.delete("all")
-                c_text(self.current, bottomCanvas)
-            else:
-                self.current = self.current[1:]
-                bottomCanvas.delete("all")
-                c_text(self.current, bottomCanvas)
-        # Handles functionality when AC button is pressed
-        if btn.cget("text") == "AC":
-            self.current = '' # Clears current number
-            self.previous = ''
-            topCanvas.delete("all") # Clear top screen
-            bottomCanvas.delete("all") # Clear bottom screen
-         
-        # Handles functionality when = button is pressed
-        if btn.cget("text") == "=":
-            if self.operator == '': # This is if no operator has been choosen
-                topCanvas.delete("all") # Delete top canvas
-                self.previous = self.current # Make current number the previous number
-                self.current = '' # Clear current number
-                bottomCanvas.delete("all") # Clear bottom screen
-                c_text(self.previous, topCanvas) # Display previous number
-            else:
-                self._do_math(c_text, topCanvas, bottomCanvas) # Display the outcome
-        
-        # Handles functionality when + button is pressed
-        if btn.cget("text") == "+":
-            if self.operator == '': # Checks to ensure only one operation is chosen (same for each operation function)
-                self._operator_input(c_text, topCanvas, bottomCanvas, "+") # Handles operation input
-        
-        # Handles functionality when - button is pressed
-        if btn.cget("text") == "-":
-            if self.operator == '':
-                self._operator_input(c_text, topCanvas, bottomCanvas, "-")
-        
-        # Handles functionality when - button is pressed
-        if btn.cget("text") == "÷":
-            if self.operator == '':
-                self._operator_input(c_text, topCanvas, bottomCanvas, "÷")
-        
-        # Handles functionality when x button is pressed
-        if btn.cget("text") == "x":
-            if self.operator == '':
-                self._operator_input(c_text, topCanvas, bottomCanvas, "x")
-        
-        # Handles functionality when x² button is pressed
-        if btn.cget("text") == "x²":
-            if self.operator == '':
-                self.operator = "x²"
-                self._do_math(c_text, topCanvas, bottomCanvas)
-          
     def init_button(self, btn, topCanvas, bottomCanvas) -> tkr.Button:
         """Initializes the buttons."""
         btn["width"] = 10
         btn["height"] = 5
         btn["bg"] = "gray19"
+        btn["bd"] = 4
+        btn["relief"] = tkr.RIDGE
         btn["fg"] = "snow"
-        btn["relief"] = tkr.GROOVE
         btn["activeforeground"] = "snow"
         btn["activebackground"] = "DarkOrchid1"
         btn.bind("<Enter>", self._on_enter)
@@ -189,13 +127,105 @@ class Calculator(tkr.Frame):
             for c in range(4): # 4 columns
                 buttons[index].grid(row=r, column=c)
                 index += 1
+# -----------------------------------------------------------------------------        
+    def _append_to_frame(self, btn, topCanvas, bottomCanvas) -> None:
+        """
+        Takes the value of the pressed-button's text and appends it
+        to the current text of the canvas.
+        """
+        canvasFont: tkrf.Font = tkrf.Font(self, family="Stencil", size="25")
+
+        c_text = (lambda text, canvas:
+                  canvas.create_text(390, 50, text=text, fill="snow",
+                                     font=canvasFont, anchor=tkr.E))
+
+        if ((str(btn.cget("text")).isnumeric()) or ((str(btn.cget("text")) == ".") and (self.current.count(".") < 1))):
+            self.current = self.current + str(btn.cget("text"))
+            self._clear(bottomCanvas)
+            c_text(self.current, bottomCanvas)
+            
+        # Handles functionality when DEL button is pressed
+        if btn.cget("text") == "DEL":
+            self.current = self.current[:-1]
+            # bottomCanvas.delete("all")
+            self._clear(bottomCanvas)
+            c_text(self.current, bottomCanvas) 
+
+        # Handles functionality when ± button is pressed
+        if btn.cget("text") == "±":
+            if (self.current != '-' or self.current == '') and self.current.count("-") < 1:
+                self.current = '-' + self.current
+                bottomCanvas.delete("all")
+                c_text(self.current, bottomCanvas)
+            else:
+                self.current = self.current[1:]
+                bottomCanvas.delete("all")
+                c_text(self.current, bottomCanvas)
+        
+        # Handles functionality when AC button is pressed
+        if btn.cget("text") == "AC":
+            self.current = '' # Clears current number
+            self.previous = ''
+            self.operator = ''
+            self._clear(bottomCanvas, topCanvas)
+         
+        # Handles functionality when = button is pressed
+        if btn.cget("text") == "=":
+            if self.operator == '': # This is if no operator has been choosen
+                self._clear(topCanvas, bottomCanvas)
+                # topCanvas.delete("all") # Delete top canvas
+                self.previous = self.current # Make current number the previous number
+                self.current = '' # Clear current number
+                # bottomCanvas.delete("all") # Clear bottom screen
+                c_text(self.previous, topCanvas) # Display previous number
+            else:
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator) # Display the outcome
+        
+        # Handles functionality when + button is pressed
+        if btn.cget("text") == "+":
+            if self.operator == '': # Checks to ensure only one operation is chosen (same for each operation function)
+                self._operator_input(c_text, topCanvas, bottomCanvas, "+") # Handles operation input
+            else:
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator)
+                self._operator_input(c_text, topCanvas, bottomCanvas, "+")
+        
+        # Handles functionality when - button is pressed
+        if btn.cget("text") == "-":
+            if self.operator == '':
+                self._operator_input(c_text, topCanvas, bottomCanvas, "-")
+            else:
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator)
+                self._operator_input(c_text, topCanvas, bottomCanvas, "-")
+        
+        # Handles functionality when - button is pressed
+        if btn.cget("text") == "÷":
+            if self.operator == '':
+                self._operator_input(c_text, topCanvas, bottomCanvas, "÷")
+            else:
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator)
+                self._operator_input(c_text, topCanvas, bottomCanvas, "÷")
+        
+        # Handles functionality when x button is pressed
+        if btn.cget("text") == "x":
+            if self.operator == '':
+                self._operator_input(c_text, topCanvas, bottomCanvas, "x")
+            else:
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator)
+                self._operator_input(c_text, topCanvas, bottomCanvas, "x")
+        
+        # Handles functionality when x² button is pressed
+        if btn.cget("text") == "x²":
+            if self.operator == '':
+                self.operator = "x²"
+                self._do_math(c_text, topCanvas, bottomCanvas, self.operator)
+          
         
     def init_frame(self) -> None:
         self.master.title("Calculator") # Sets title of the frame to "Calculator"
-        buttonFrame: tkr.Frame = tkr.Frame(self, height=100, width=200, bg="gray25", bd=0, relief=tkr.GROOVE) # Frame that holds the buttons
-        canvasFrame: tkr.Frame = tkr.Frame(self, height=200, width=200, bg="gray25", bd=0, relief=tkr.GROOVE) # Frame that holds the screen/canvas
-        topCanvas: tkr.Frame = tkr.Canvas(canvasFrame, width=200, height=100, bg="gray25", bd=0, relief=tkr.GROOVE) # Where inputed numbers and outputs will be displayed
-        bottomCanvas: tkr.Frame = tkr.Canvas(canvasFrame, width=200, height=100, bg="gray25", bd=0, relief=tkr.GROOVE)
+        buttonFrame: tkr.Frame = tkr.Frame(self, height=100, width=200, bg="gray25", bd=0) # Frame that holds the buttons
+        canvasFrame: tkr.Frame = tkr.Frame(self, height=200, width=200, bg="gray25", bd=5, relief=tkr.RIDGE) # Frame that holds the screen/canvas
+        topCanvas: tkr.Frame = tkr.Canvas(canvasFrame, width=200, height=100, bg="gray25", highlightthickness=0) # Where inputed numbers and outputs will be displayed
+        bottomCanvas: tkr.Frame = tkr.Canvas(canvasFrame, width=200, height=100, bg="gray25", highlightthickness=0)
         
         buttonFont: tkrf.Font = tkrf.Font(self, family="Century Gothic", size="11")
         buttonFrame.pack(side=tkr.BOTTOM, expand=tkr.TRUE, fill=tkr.X)
